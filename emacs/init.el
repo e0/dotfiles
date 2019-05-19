c1769055d87f38252247fc841b6df949d44dd0b7
e0 <shan.huang@me.com>
2019-05-19 16:54:26 +0200

    Clean up emacs init file

--- emacs/init.el
+++ emacs/init.el
@@ -1,51 +1,44 @@
+; packages
 (require 'package)
-
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
-(set-face-attribute 'default nil :height 140)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes (quote (solarized-light)))
- '(custom-safe-themes
-   (quote
-    ("d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" default)))
- '(desktop-save-mode t nil (desktop))
- '(package-selected-packages
-   (quote
-    (magit neotree helm cider clojure-mode solarized-theme)))
- '(save-place-mode t nil (saveplace)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq package-list
+      '(magit neotree helm cider clojure-mode solarized-theme))
 
 (unless package-archive-contents
   (package-refresh-contents))
-(package-install-selected-packages)
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+					
+; visual
+(set-face-attribute 'default nil :font "IBM Plex Mono" :height 140)
+(load-theme 'solarized-light t)
+(global-display-line-numbers-mode)
+(tool-bar-mode -1)
 
 
-(setq apropos-sort-by-scores t)
+; keys
+(global-set-key (kbd "M-o") 'other-window)
+(global-set-key (kbd "C-x g") 'magit-status)
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier nil)
 
-(global-set-key (kbd "M-o") 'other-window)
-(global-display-line-numbers-mode)
+
+; other
+(desktop-save-mode 1)
+(save-place-mode 1)
+
+(setq apropos-sort-by-scores t)
 
 (add-to-list 'exec-path "/usr/local/bin")
 
 (require 'helm-config)
-
 (ido-mode 1)
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
+
+
